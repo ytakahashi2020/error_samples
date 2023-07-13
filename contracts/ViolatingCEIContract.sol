51ae1e28c170da19b3c908e4ae73ad13d87bebaf
ytakahashi2020 <y.takahashi2020@gmail.com>
2023-07-13 14:45:51 +0900

    add cei

---
+++ contracts/ViolatingCEIContract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+contract ViolatingCEIContract {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Not enough balance");
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+        balances[msg.sender] -= _amount;
+    }
+}
