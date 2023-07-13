8f6a5bd81a6e62d8513b8a1fba7e66aaaeeacf7b
ytakahashi2020 <y.takahashi2020@gmail.com>
2023-07-13 13:58:33 +0900

    add EtherStore2

---
+++ contracts/EtherStore2.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract EtherStore2 is ReentrancyGuard {
+    mapping(address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public nonReentrant {
+        uint bal = balances[msg.sender];
+        require(bal > 0);
+
+        (bool sent, ) = msg.sender.call{value: bal}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] = 0;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
